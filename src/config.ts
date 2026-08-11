63102c31621f62d593eb5a2e160d0ed774c08df5
iamluieseedu <iamluieseedu@users.noreply.github.com>
2026-08-11 15:48:37 +0800

    Embed Google Sheets Webhook URL into global static config defaults

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // Students on any device or profile will load these settings by default.
 export const CONFIG = {
   // Paste your Google Sheets Webhook URL here:
-  webhookUrl: '',
+  webhookUrl: 'https://script.google.com/a/macros/faculty.trimexcolleges.edu.ph/s/AKfycbyCoQCeJzEjCuYFKARWqCFC7jGLFujyHutn0gmLebrElOk8nzIPx3Begv-VaiOKWnrpIg/exec',
   
   // Paste your Google Cloud Client ID here:
   googleClientId: '1033435332000-dn7dhr38rlpe1e9itgcf48qhdlu4dkck.apps.googleusercontent.com',
